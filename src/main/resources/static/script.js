2fb1a9b2932d4dfee1f8bf5925ede3c7f6868904
Mike <mikelautensack100@gmail.com>
2022-12-20 23:13:46 -0500

    started adding the legalmove logic

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -6,8 +6,39 @@ let currentBoardConfig = {
 
 }
 
-let validMoves = {
-
+let legalMoves = {
+    'whiteKing' : [],
+    'whiteQueen' : [],
+    'whiteKsRook' : [],
+    'whiteQsRook' : [],
+    'whiteLsBishop' : [],
+    'whiteDsBishop' : [],
+    'whiteKsKnight' : ['f3','h3'],
+    'whiteQsKnight' : ['a3','c3'],
+    'whiteAPawn' : ['a3','a4'],    
+    'whiteBPawn' : ['b3','b4'],
+    'whiteCPawn' : ['c3','c4'],
+    'whiteDPawn' : ['d3','d4'],
+    'whiteEPawn' : ['e3','e4'],
+    'whiteFPawn' : ['f3','f4'],
+    'whiteGPawn' : ['g3','g4'],
+    'whiteHPawn' : ['h3','h4'],
+    'blackKing' : [],
+    'blackQueen' : [],
+    'blackKsRook' : [],
+    'blackQsRook' : [],
+    'blackLsBishop' : [],
+    'blackDsBishop' : [],
+    'blackKsKnight' : ['f6','h6'],
+    'blackQsKnight' : ['a6','c6'],
+    'blackAPawn' : ['a6','a5'],    
+    'blackBPawn' : ['b6','b5'],
+    'blackCPawn' : ['c6','c5'],
+    'blackDPawn' : ['d6','d5'],
+    'blackEPawn' : ['e6','e5'],    
+    'blackFPawn' : ['f6','f5'],
+    'blackGPawn' : ['g6','g5'],
+    'blackHPawn' : ['h6','h5'],
 }
 
 let squaresControlledByWhite = {
@@ -19,8 +50,10 @@ let squaresControlledByBlack = {
 }
 
 let isWhitesTurn = true;
-
 let pieceMoving;
+let pieceMovingID;
+let squareID;
+let pieceMovingLegalMoves;
 
 //piece listeners
 pieces.forEach(piece => {
@@ -50,8 +83,16 @@ squares.forEach(square => {
     })*/
 
     square.addEventListener('drop', () => {
+
+        //turn logic
         if(isWhitesTurn == true) {
             if(pieceMoving.hasAttribute('data-white')) {
+                pieceMovingID = pieceMoving.id;
+                squareID = square.id;
+                pieceMovingLegalMoves = legalMoves.pieceMovingID;
+                /*if(pieceMovingLegalMoves.includes(squareID)) {
+
+                }*/
                 square.append(pieceMoving);
                 isWhitesTurn = false;
             }
@@ -60,8 +101,10 @@ squares.forEach(square => {
                 square.append(pieceMoving);
                 isWhitesTurn = true;
             }
-            console.log(isWhitesTurn);
         }
+        console.log(pieceMovingLegalMoves);
+        //valid squares logic
+
     })    
 })
 
