1ded11c2dc152f1371bc199ff0ef6fd57140ae5a
Mike <mikelautensack100@gmail.com>
2023-01-08 12:32:38 -0500

    test

--- src/main/java/com/Game/Chess/Model/board/Board.java
+++ src/main/java/com/Game/Chess/Model/board/Board.java
@@ -1,7 +1,9 @@
 package com.Game.Chess.Model.board;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 
+import com.Game.Chess.Model.piece.Piece;
 import com.Game.Chess.ResourceRepresentationClasses.BoardConfig;
 
 public class Board {
@@ -29,8 +31,26 @@ public class Board {
                 boardArray[i][fileIndex] = newSquare;
 
                 // Build piece and add to square
+
+                // Get square coordinate
                 Coordinates squareCoordinate = newSquare.getSquareCoordinate();
                 String coordinate = squareCoordinate.getCoordinate();
+
+                // Get value of the instance variable of the boardConfig passed in that matches
+                // square coordinate
+                Field[] boardConfigFields = boardConfig.getClass().getDeclaredFields();
+                for (Field field : boardConfigFields) {
+                    Field f = boardConfig.getClass().getDeclaredField(field);
+                    if (field.getName().equals(coordinate)) {
+                        String boardConfigPieceValue = field.get(field);
+                    }
+                }
+
+                // Build piece object that represents the value you got from the previus step
+
+                // Set pieceOnSquare of newSqaure to the piece you build in the previus step
+
+                Piece piece = boardArray[i][fileIndex].getPieceOnSquare();
 
                 // Change square color
                 color = (color == SquareColor.DARK) ? SquareColor.LIGHT : SquareColor.DARK;
